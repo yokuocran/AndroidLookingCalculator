79a241ad3d2fbb154f3309015718a1731331b5be
Yoku Ocran <yokuocran@gmail.com>
2020-05-18 00:54:11 +0000

    First Commit

---
+++ AndroidLookingCalculator/AndroidLookingCalculator.Android/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidLookingCalculator.Controls;
+using AndroidLookingCalculator.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer (typeof(CustomEntry), (typeof(CustomEntryRenderer)))]
+namespace AndroidLookingCalculator.Droid.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+            {
+                Control.Background = null;
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ AndroidLookingCalculator/AndroidLookingCalculator/MainPage.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AndroidLookingCalculator
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private string operandOne { get; set; }
+        private string operandTwo { get; set; }
+        private string operation { get; set; }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Op_Tapped(object sender, EventArgs e)
+        {
+            var opLbl = sender as Label;
+
+            if (opLbl.Text == "Clear")
+                clearAll();
+            else
+            {
+                calculationLbl.Text += opLbl.Text;
+                operation = opLbl.Text;
+                if (!string.IsNullOrEmpty(displayLbl.Text))
+                {
+                    operandOne = displayLbl.Text;
+                    operandTwo = string.Empty;
+                }
+            }
+        }
+
+        private void numKey_Tapped(object sender, EventArgs e)
+        {            
+            var lbl = sender as Label;
+
+            if (lbl.Text == "=")
+            {
+                calculationLbl.Text = displayLbl.Text;
+                displayLbl.Text = string.Empty;
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(operation))
+                    operandOne += lbl.Text;
+                else
+                    operandTwo += lbl.Text;
+
+                calculationLbl.Text += lbl.Text;
+
+                if (!string.IsNullOrEmpty(operandTwo))
+                {
+                    Evaluate();
+                }
+            }
+        }
+
+        private void Evaluate()
+        {
+            string sum = string.Empty;
+
+            try
+            {
+                var one = int.Parse(operandOne);
+                var two = int.Parse(operandTwo);
+
+                switch (operation)
+                {
+                    case "-":
+                        sum = (one - two).ToString();
+                        break;
+                    case "+":
+                        sum = (one + two).ToString();
+                        break;
+                    case "x":
+                        sum = (one * two).ToString();
+                        break;
+                    case "/":
+                        sum = (one / two).ToString();
+                        break;
+                    case "Clear":
+                        clearAll();
+                        break;
+                    default:
+                        sum = "Bad Expression";
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                sum = "Bad Expression";
+            }
+            finally
+            {
+                displayLbl.Text = sum;
+            }
+        }
+
+        private void clearAll()
+        {
+            displayLbl.Text = string.Empty;
+            calculationLbl.Text = string.Empty;
+            operandOne = string.Empty;
+            operandTwo = string.Empty;
+            operation = string.Empty;
+        }
+    }
+}
